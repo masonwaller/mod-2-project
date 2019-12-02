57053fcacd9752a7e5d86799d42eeb319156b3be
masonwaller <masonwaller805@gmail.com>
2019-12-02 16:13:04 -0600

    users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :dogs
     has_many :comments
     has_many :parks, through: :comments
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
 end
