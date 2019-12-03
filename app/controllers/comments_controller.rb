21a0fe95f9c7129d582d7dd77a5d7a2bbd43ec93
faithe1937 <faithe1937@gmail.com>
2019-12-03 16:22:40 -0600

    created comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,36 @@
 class CommentsController < ApplicationController
     before_action :find_book
+    before_action :find_comment, only: [:edit, :update, :destroy]
     def new 
         @comment = Comment.new
     end 
 
     def create 
         @comment = Comment.new(comment_params)
+        #associate a comment with the current park
         @comment.park_id = @park.id
-        @user.user_id = current_user.id
+        @comment.user_id = current_user.id
 
         if @comment.save
-            redirect_to @park
+            redirect_to park_path(@park)
         else
             render :new
+    end 
+
+    def edit
+    end
+
+    def update
+        if @comment.update(comment_params)
+            redirect_to park_path(@park)
+        else 
+            render :edit
+        end
+    end 
+
+    def destroy
+        @comment.destroy
+        redirect_to park_path(@park)
     end 
 
 private 
@@ -24,5 +42,9 @@ def find_park
     @park = Park.find(params[park.id])
 end
 
+#finding the current park that the comment is associated with (book_id)
+def find_comment
+    @comment = Comment.find(params[:id])
 end 
 
+end 
