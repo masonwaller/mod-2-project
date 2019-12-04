fa3acb8328109d19391395af55b53b32482e02db
masonwaller <masonwaller805@gmail.com>
2019-12-04 13:31:52 -0600

    fixed user login

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_194712) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
