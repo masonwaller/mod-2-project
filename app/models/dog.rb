85b38e3a27469e63fe802ee31e7c2e1f29cfd760
masonwaller <masonwaller805@gmail.com>
2019-12-06 11:25:46 -0600

    made sure a dog had a profile picture

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,6 +5,7 @@ class Dog < ApplicationRecord
   validates :name, presence: true
   validates :user, presence: true
   validates :bio, presence: true
+  validates :avatar, presence: true
 
   def image_resize
     self.avatar.variant(resize: "250x250")
