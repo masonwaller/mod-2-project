9287710624201940aa595bac0ab136902d73c09a
github username <github email address>
2020-01-13 14:35:37 -0600

    heroku database

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_12_03_194712) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,8 +38,8 @@ ActiveRecord::Schema.define(version: 2019_12_03_194712) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.integer "user_id", null: false
-    t.integer "park_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "park_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["park_id"], name: "index_comments_on_park_id"
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_194712) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "bio"
